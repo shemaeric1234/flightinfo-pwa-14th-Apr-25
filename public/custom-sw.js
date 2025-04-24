a2c2b42a50d11248112d5aac1e15007ebd86e3eb
shemaeric1234 <shemaerc@gmail.com>
2025-04-24 19:41:41 +0400

    update cash version to 1.9

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "new-flightinfo-pwa-v1.8";
+const CACHE_NAME = "new-flightinfo-pwa-v1.9";
 
 const urlsToCache = [
   "https://shemaeric1234.github.io/flightinfo-pwa-14th-Apr-25/",
